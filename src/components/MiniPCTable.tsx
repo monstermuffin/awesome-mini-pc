4f13e8455d3a2b76ac8625c95a078f7b86208099
muffin <MonsterMuffin@users.noreply.github.com>
2025-04-01 20:26:44 +0100

    fix: remove theme var

--- src/components/MiniPCTable.tsx
+++ src/components/MiniPCTable.tsx
@@ -38,7 +38,6 @@ type SortConfig = {
 };
 
 export function MiniPCTable({ devices }: MiniPCTableProps) {
-  const theme = useTheme();
   const [sortConfig, setSortConfig] = useState<SortConfig>({
     key: 'brand',
     direction: 'asc',
