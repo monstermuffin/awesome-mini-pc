77c1ffb64b0fa19174f403b7f9136c54d09bd239
muffin <MonsterMuffin@users.noreply.github.com>
2025-04-02 17:09:34 +0100

    ci: fixes

--- src/App.tsx
+++ src/App.tsx
@@ -509,7 +509,7 @@ function App() {
               <Tooltip title="View on GitHub">
                 <IconButton
                   component={Link}
-                  href="https://github.com/yourusername/awesome-mini-pcs"
+                  href="https://github.com/monstermuffin/awesome-mini-pc/"
                   target="_blank"
                   rel="noopener noreferrer"
                   color="inherit"
